3c9cce66223e25fef6fcc12435b801afce569372
Artyom Mikhailov <happytoad42@gmail.com>
2019-08-18 22:04:40 +0700

    deployed on netlify, changed readme

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,9 +35,7 @@ const GithubState = props => {
   const searchUsers = async text => {
     setLoading();
     const res = await Axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${gitgubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: SEARCH_USERS,
@@ -48,9 +46,7 @@ const GithubState = props => {
   const getUser = async username => {
     setLoading();
     const res = await Axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${gitgubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -63,9 +59,7 @@ const GithubState = props => {
   const getUserRepos = async username => {
     setLoading();
     const res = await Axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${gitgubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
